77fd9ea4b426058eb6a6dc9e3657232fbca2b17b
Vladyslav Vorobiov <vlad65e@gmail.com>
2024-11-17 14:53:18 +0200

    test(details): add unit tests

--- src/app/pages/details/details.component.spec.ts
+++ src/app/pages/details/details.component.spec.ts
@@ -1,23 +1,78 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, provideRouter, Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
+import { FavoritePhotoService } from 'core-api';
 import { DetailsComponent } from './details.component';
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
+  let favoritePhotoServiceMock: FavoritePhotoService;
+  let routerMock: Router;
+
+  const getHtmlElementDebug = (cssClass: string): DebugElement =>
+    fixture.debugElement.query(By.css(cssClass));
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DetailsComponent]
-    })
-    .compileComponents();
+      imports: [DetailsComponent],
+      providers: [
+        provideRouter([]),
+        {
+          provide: FavoritePhotoService,
+          useValue: {
+            getById: () => ({ id: 'mockId', url: 'mockUrl' }),
+            remove: () => {},
+          },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { params: { id: 'mockId' } } },
+        },
+        {
+          provide: Router,
+          useValue: { navigate: () => {} },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
+
+    favoritePhotoServiceMock = TestBed.inject(FavoritePhotoService);
+    routerMock = TestBed.inject(Router);
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('When is initialized', () => {
+    it('should create component', () => {
+      expect(component).toBeTruthy();
+    });
+
+    describe('when click on remove button', () => {
+      let spyOnRemove: jasmine.Spy;
+      let spyOnNavigate: jasmine.Spy;
+
+      beforeEach(() => {
+        spyOnRemove = spyOn(favoritePhotoServiceMock, 'remove');
+        spyOnNavigate = spyOn(routerMock, 'navigate');
+
+        const removeButton = getHtmlElementDebug('.actions button');
+        removeButton.triggerEventHandler('click');
+
+        fixture.detectChanges();
+      });
+
+      it('should call remove logic', () => {
+        expect(spyOnRemove).toHaveBeenCalled();
+      });
+
+      it('should call navigate logic', () => {
+        expect(spyOnNavigate).toHaveBeenCalled();
+      });
+    });
   });
 });
