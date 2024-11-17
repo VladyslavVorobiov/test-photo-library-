25849710f0ad94281fa6092d4d13a4e054ab18ce
Vladyslav Vorobiov <vlad65e@gmail.com>
2024-11-17 15:24:20 +0200

    test(favorites): add unit tests

--- src/app/pages/favorites/favorites.component.spec.ts
+++ src/app/pages/favorites/favorites.component.spec.ts
@@ -1,22 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { DebugElement } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
+import { PhotoItem } from 'core-api';
 import { FavoritesComponent } from './favorites.component';
+import { DataService } from './services';
+
+const MOCK_PHOTOS: PhotoItem[] = [
+  { id: 'mockId1', url: 'mockUrl1' },
+  { id: 'mockId2', url: 'mockUrl2' },
+];
 
 describe('FavoritesComponent', () => {
   let component: FavoritesComponent;
   let fixture: ComponentFixture<FavoritesComponent>;
 
+  const getHtmlElementsDebug = (cssClass: string): DebugElement[] =>
+    fixture.debugElement.queryAll(By.css(cssClass));
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FavoritesComponent],
-    }).compileComponents();
+      imports: [FavoritesComponent, AsyncPipe],
+      providers: [
+        {
+          provide: Router,
+          useValue: { navigate: () => {} },
+        },
+      ],
+    })
+      .overrideProvider(DataService, {
+        useValue: {
+          data$: of(MOCK_PHOTOS),
+          loadData: () => {},
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(FavoritesComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('When is initialized', () => {
+    it('should create component', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should render proper amount of photos', () => {
+      const photos = getHtmlElementsDebug('.image-item');
+
+      expect(photos.length).toBe(MOCK_PHOTOS.length);
+    });
   });
 });
--- src/app/pages/photos/photos.component.spec.ts
+++ src/app/pages/photos/photos.component.spec.ts
@@ -2,15 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotosComponent } from './photos.component';
 
+// TODO: for this component makes sense to create e2e tests
+// to check features in browser environment
 describe('PhotosComponent', () => {
   let component: PhotosComponent;
   let fixture: ComponentFixture<PhotosComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PhotosComponent]
-    })
-    .compileComponents();
+      imports: [PhotosComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PhotosComponent);
     component = fixture.componentInstance;
