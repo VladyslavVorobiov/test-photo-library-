2896e0610021382a4b12856f619107f8fdf2b822
Vladyslav Vorobiov <vlad65e@gmail.com>
2024-11-17 14:21:42 +0200

    test(navigation): add unit tests

--- src/app/core/components/navigation/navigation.component.spec.ts
+++ src/app/core/components/navigation/navigation.component.spec.ts
@@ -1,23 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement, Type } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 import { NavigationComponent } from './navigation.component';
 
+// This approach with HostComponent is useful when
+// we want to test a component that is used in another component
+// we want to test a component's inputs and outputs
+// we want to test a component's public properties and methods
+@Component({
+  standalone: true,
+  template: `<app-navigation />`,
+  imports: [NavigationComponent],
+})
+class TestHostComponent {}
+
 describe('NavigationComponent', () => {
-  let component: NavigationComponent;
-  let fixture: ComponentFixture<NavigationComponent>;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let hostComponent: TestHostComponent;
+  let navigationComponent: NavigationComponent;
+
+  const getComponentDebug = (type: Type<unknown>): DebugElement =>
+    fixture.debugElement.query(By.directive(type));
+
+  const getHtmlElementsDebug = (cssClass: string): DebugElement[] =>
+    fixture.debugElement.queryAll(By.css(cssClass));
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavigationComponent]
-    })
-    .compileComponents();
+      imports: [TestHostComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(NavigationComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = fixture.componentInstance;
     fixture.detectChanges();
+
+    navigationComponent =
+      getComponentDebug(NavigationComponent).componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('When is initialized', () => {
+    it('should create component', () => {
+      expect(navigationComponent).toBeTruthy();
+    });
+
+    it('should render proper amount of navigation links', () => {
+      const navigationLinks = getHtmlElementsDebug('.navigation-link');
+
+      expect(navigationLinks.length).toBe(
+        navigationComponent.navigationItems.length
+      );
+    });
   });
 });
